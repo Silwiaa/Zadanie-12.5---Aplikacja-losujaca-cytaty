b99efa440d6a8123b437308f79e4957bb07600e5
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-23 18:47:06 +0200

    finish CreateTweet function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
-// VARIABLE tweetLink AND quoteuRL
-var tweetLink = "https://twitter.com/intent/tweet?text=",
-    quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-
-// getQuote FUNCTION
-function getQuote() {
-    $getJSON(quoteUrl, createTweet);
-}
-
-// createTweet FUNCTION
-function createTweet(input) {
-    var data = input(0),
-        quoteText = $(input.content).text().trim(),
-        quoteAuthor = data.title;
+$(function() {
     
-    if(!quoteAuthor.lenght) {
-        quoteAuthor = "Unknown author";
+    // VARIABLE tweetLink AND quoteuRL
+    var tweetLink = "https://twitter.com/intent/tweet?text=",
+        quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+    // getQuote FUNCTION
+    function getQuote() {
+        $getJSON(quoteUrl, createTweet);
     }
-}
+
+    // createTweet FUNCTION
+    function createTweet(input) {
+        var data = input(0),
+            quoteText = $(input.content).text().trim(),
+            quoteAuthor = data.title;
+
+        if(!quoteAuthor.lenght) {
+            quoteAuthor = "Unknown author";
+        }
+
+        var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+        if (tweetText.length > 140) {
+        getQuote();
+        } else {
+            var tweet = tweetLink + encodeURIComponent(tweetText);
+            
+            $('.quote').text(quoteText);
+            $('.author').text("Author:" + quoteAuthor);
+            $('.tweet').attr('href', tweet);
+        }
+    }
+});
