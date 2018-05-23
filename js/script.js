270981d2e78584e006c1934a82c7e1333129fcbd
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-23 18:25:49 +0200

    Create createTweet variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 // VARIABLE tweetLink AND quoteuRL
 var tweetLink = "https://twitter.com/intent/tweet?text=",
     quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+// getQuote FUNCTION
+function getQuote() {
+    $getJSON(quoteUrl, createTweet);
+}
+
+// createTweet FUNCTION
+function createTweet(input) {
+    var data = input(0),
+        quoteText = $(input.content).text().trim(),
+        quoteAuthor = data.title;
+    
+    if(!quoteAuthor.lenght) {
+        quoteAuthor = "Unknown author";
+    }
+}
