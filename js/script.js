5cdceeec04771d66c2b71be94bf7570e2aac70f0
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-23 18:15:50 +0200

    Add tweetLink and quoteUrl variables

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// VARIABLE tweetLink AND quoteuRL
+var tweetLink = "https://twitter.com/intent/tweet?text=",
+    quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
