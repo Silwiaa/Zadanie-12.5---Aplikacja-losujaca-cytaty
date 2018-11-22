bcebbdd87efef8af6230ad8d8317c3ba2c330765
Sylwia <35842428+Silwiaa@users.noreply.github.com>
2018-11-22 17:36:21 +0100

    upgrade script.js

--- js/script.js
+++ js/script.js
@@ -1,43 +1,37 @@
-$(function () {
-    // VARIABLE tweetLink AND quoteuRL
-    var prefix = "https://cors-anywhere.herokuapp.com/",
-        tweetLink = "https://twitter.com/intent/tweet?text=",
-        quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var tweetLink = "https://twitter.com/intent/tweet?text=",
+    quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
-    // getQuote FUNCTION
-    function getQuote() {
-        $.getJSON(prefix + quoteUrl, createTweet);
-        $.ajaxSetup({ cache: false });
+// $.getJSON() METHOD
+function getQuote() {
+    $.getJSON(quoteUrl, createTweet);
+}
+
+//CREATE NEW TWEETER LINKS
+function createTweet(input) {
+    var data = input[0],
+        quoteText = $(data.content).text().trim(),
+        quoteAuthor = data.title;
+    
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
     }
     
-    // SET getQuote FUNCTION WHEN WEB LOADED
-    $(document).ready(function () {
-        getQuote();
-    $('.trigger').click(function () {
-        getQuote();        
-        });
-    });
-
-    // createTweet FUNCTION
-    function createTweet(input) {
-        var data = input[0],
-            quoteText = $(input.content).text().trim(),
-            quoteAuthor = data.title;
-
-        if (!quoteAuthor.lenght) {
-            quoteAuthor = " Unknown author";
-        }
-
-        var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-
-        if (tweetText.length > 140) {
-        getQuote();
-        } else {
-            var tweet = tweetLink + encodeURIComponent(tweetText);
-            
-            $('.quote').text(quoteText);
-            $('.author').text("Author:" + quoteAuthor);
-            $('.tweet').attr('href', tweet);
-        }
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+    
+    if (tweetText.length > 140) {
+    getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        $('.quote').text(quoteText);
+        $('.author').text("Author: " + quoteAuthor);
+        $('.tweet').attr('href', tweet);
     }
+}
+
+//GENERATE NEW QUOTE
+$(document).ready(function() {
+    getQuote();
+    $('trigger').click(function () {
+        getQuote();
+    })
 });
